516d336d15c7783b288256bec73b4fa49179a141
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-05-12 10:07:18 +0530

    Create auth.js

---
+++ Backend/config/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({
+    path:"../config/.env"
+})
+
+const isAuthenticated = async(req,res,next) => {
+    try{
+        const token = req.cookies.token;
+        console.log(token);
+        if(!token){
+            return res.status(401).json({
+                message:"User not authenticated.",
+                success:false
+            })
+        }
+        const decode = await jwt.verify(token,process.env.TOKEN_SECRET);
+        console.log(decode);
+        req.user=decode.userId;
+        next();
+
+    }
+    catch(error){
+console.log(error);
+    }
+}
+export default isAuthenticated;
