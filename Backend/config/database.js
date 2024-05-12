bd507389704096a26e33fb34d0b42ec33de0d677
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-05-12 10:07:52 +0530

    Add files via upload

---
+++ Backend/config/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({
+    path:"../config/.env"
+})
+
+const databaseConnection=()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("Connected to MongoDB");
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
+
+export default databaseConnection;
